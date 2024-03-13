10f41186d288723d086269097a03f11e56ec3676
1VEAO <alarconolmedo99@gmail.com>
2024-03-12 19:45:15 -0500

    Update form-action.js

--- scripts/form-action.js
+++ scripts/form-action.js
@@ -1,9 +1,9 @@
 let inputNombre = document.getElementById('nombre')
 let inputEmail = document.getElementById('email')
-let textAreaMensaje = document.getElementById('mensaje')
-let buttom = document.querySelector('button')
+// let textAreaMensaje = document.getElementById('mensaje')
+// let buttom = document.querySelector('button')
 let formulario = document.getElementsByClassName('formulario')[0]
-let inputL = document.querySelector('input').length
+// let inputL = document.querySelector('input').length
 // inputNombre.inputMode = function () {
 //  console.log(inputNombre.value);
 // }
@@ -13,11 +13,16 @@ let inputL = document.querySelector('input').length
 
 formulario.addEventListener('submit', (e)=>{
  e.preventDefault()
- if (inputNombre.value.length <= 4 & inputEmail.value.length <= 9) {
+ if (inputNombre.value.length <= 4 && inputEmail.value.length <= 9) {
   inputNombre.value = ''
   inputNombre.placeholder = 'ingresa tu nombre'
   inputEmail.placeholder = 'ingresa un correo valido'
- } else{
+ } else if (inputNombre.value.length <= 4 && inputEmail.value.length >= 9) {
+  inputNombre.value = ''
+  inputNombre.placeholder = 'ingresa tu nombre'
+ } else if (inputEmail.value.length <= 9 && inputNombre.value.length >= 4){
+  inputEmail.placeholder = 'ingresa un correo valido'
+ } else {
   alert('enviado')
   e.currentTarget.submit()
  }
